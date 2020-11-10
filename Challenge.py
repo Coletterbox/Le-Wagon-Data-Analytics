a95355618d4beb4116d54c2ca2bf1c49e5597646
Colette <38261091+Coletterbox@users.noreply.github.com>
2020-11-10 21:21:12 +0000

    Update Challenge.py

--- Challenge.py
+++ Challenge.py
@@ -9,3 +9,9 @@ tips_df.head(10)
 
 # find no. days per week that the restaurant is open
 len(tips_df['day'].unique())
+
+# find day of the week where there are more bills
+tips_df['day'].value_counts()
+
+# plot this with a Seaborn Countplot
+sns.countplot(data=tips_df, x='day', order=['Thur', 'Fri', 'Sat', 'Sun'])
